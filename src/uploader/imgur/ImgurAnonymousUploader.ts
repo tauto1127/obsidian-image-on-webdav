1046a0580788177552fe0eb7fbcef42595e0a281
git <test@example.com>
2022-04-11 11:54:31 +0200

    working alpha version

--- src/uploader/imgur/ImgurAnonymousUploader.ts
+++ src/uploader/imgur/ImgurAnonymousUploader.ts
@@ -1,9 +1,8 @@
-import NextcloudClient from "nextcloud-link";
 import { Readable } from "stream";
 import { ImgurPostData } from "../../imgur/imgurResponseTypes";
 import ImageUploader from "../ImageUploader";
 import { handleImgurErrorResponse } from "../../imgur/ImgurClient";
-
+import { requestUrl } from "obsidian";
 
 export default class ImgurAnonymousUploader implements ImageUploader {
   private readonly clientId!: string;
@@ -11,7 +10,12 @@ export default class ImgurAnonymousUploader implements ImageUploader {
   private readonly clientUrl!: string;
   private readonly clientPath!: string;
 
-  constructor(clientId: string, clientPassword: string, clientUrl: string, clientPath: string) {
+  constructor(
+    clientId: string,
+    clientPassword: string,
+    clientUrl: string,
+    clientPath: string
+  ) {
     this.clientId = clientId;
     this.clientPassword = clientPassword;
     this.clientUrl = clientUrl;
@@ -19,18 +23,6 @@ export default class ImgurAnonymousUploader implements ImageUploader {
   }
 
   async upload(image: File): Promise<string> {
-    const client = new NextcloudClient({
-      url: this.clientUrl,
-      password: this.clientPassword,
-      username: this.clientId,
-    });
-
-    const vbuf = await image.arrayBuffer();
-    const newStream = new Readable({
-      read() {
-        this.push(vbuf);
-      },
-    });
     const rndf = () => {
       const lst =
         "abdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY0123456789";
@@ -42,9 +34,40 @@ export default class ImgurAnonymousUploader implements ImageUploader {
         )
         .join("");
     };
-    const cPath = `${this.clientPath}/${rndf()}.png`;
-    const resp = await client.uploadFromStream(cPath, newStream);
-    const lnkResp = await client.shares.add(cPath, 3);
-    return lnkResp.url + "/preview";
+    const innerpath = `${this.clientPath}/${rndf()}.png`;
+    const cPath = `remote.php/dav/files/${this.clientId}${innerpath}`;
+    const bufx = await image.arrayBuffer();
+    const fpUpl = await requestUrl({
+      url: `${this.clientUrl}/${cPath}`,
+      method: "PUT",
+      headers: {
+        Authorization:
+          "Basic " +
+          Buffer.from(this.clientId + ":" + this.clientPassword).toString(
+            "base64"
+          ),
+      },
+      body: bufx,
+    });
+
+    const lnkResp = await requestUrl({
+      url: `${this.clientUrl}/ocs/v2.php/apps/files_sharing/api/v1/shares`,
+      method: "POST",
+      headers: {
+        "OCS-APIRequest": "true",
+        Authorization:
+          "Basic " +
+          Buffer.from(this.clientId + ":" + this.clientPassword).toString(
+            "base64"
+          ),
+        "Content-Type": "application/json;charset=UTF-8",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        path: innerpath,
+        shareType: 3,
+      }),
+    });
+    return lnkResp.json.ocs.data.url + "/preview";
   }
 }
